75cac2a94e7c67551a820df2505ac7976fafa0b6
Nandini Tarafdar <nandinitarafdar481@gmail.com>
2026-03-20 13:49:54 +0530

    creating products

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -8,15 +7,3 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(<BrowserRouter>
   <App/>
 </BrowserRouter>);
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> f327d702697e9bdfdd0a7678ea4edc464238d686
--- src/pages/products/product-list.tsx
+++ src/pages/products/product-list.tsx
@@ -11,7 +11,7 @@ const Productlist = () => {
   return (<div>
     <div>
       <div className="border rounded-lg  shadow-sm p-2 flex">
-        <div className="w-1/2">
+        <div className="mr-4">
           <img src="/assets/necklace.png" alt="black" className=" bg-amber-100"></img>
         </div>
         <div>
@@ -21,7 +21,27 @@ const Productlist = () => {
           </p>
           <p className="text-sm text-gray-500">Yemelary Wings Diamond</p>
           <p>
-            Gold jewellery is a timeless, durable, and highly prized luxury metal known for its luster, malleability, and ability to hold value over centuries. Ranging from 10k to 24k purity, it is often alloyed with other metals for strength, with 14k and 18k being popular for daily wear while 22k is preferred for traditional, intricate pieces.{" "}
+            Gold jewellery is a timeless, durable, and highly prized luxury metal known for its luster, malleability, and ability to hold value over centuries
+          </p>
+          <button className="text-blue-500 mr-5" onClick={goToAbout}>
+            know more
+          </button>
+        </div>
+      </div>
+
+      <div className="border rounded-lg  shadow-sm p-2 flex">
+        <div className="mr-4">
+          <img src="/assets/jewel2.jpg
+          " alt="black" className=" bg-amber-100 size-56"></img>
+        </div>
+        <div>
+          <p className="mt-2 font-semibold">22,32,336 /-</p>
+          <p className="text-green-600 text-sm font-semibold">
+            10% off on making charges
+          </p>
+          <p className="text-sm text-gray-500">Yemelary Wings Diamond</p>
+          <p>
+            Gold jewellery is a timeless, durable, and highly prized luxury metal known for its luster, malleability, and ability to hold value over centuries
           </p>
           <button className="text-blue-500 mr-5" onClick={goToAbout}>
             know more
